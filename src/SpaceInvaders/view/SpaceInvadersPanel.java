22277303286667543a47b9690f47b63beb62eaef
peterlabick <peterlabick@gmail.com>
2022-01-01 19:18:43 -0500

    Refactor paintComponent with helper methods

--- src/SpaceInvaders/view/SpaceInvadersPanel.java
+++ src/SpaceInvaders/view/SpaceInvadersPanel.java
@@ -1,9 +1,9 @@
 package SpaceInvaders.view;
 
 import SpaceInvaders.Constants;
+import SpaceInvaders.model.SpaceInvadersBoard;
 import SpaceInvaders.model.piece.Alien;
 import SpaceInvaders.model.piece.Bullet;
-import SpaceInvaders.model.SpaceInvadersBoard;
 
 import javax.swing.*;
 import java.awt.*;
@@ -22,36 +22,43 @@ public class SpaceInvadersPanel extends JPanel {
     this.setBackground(Constants.BACKGROUND_COLOR);
     g.setColor(Constants.FOREGROUND_COLOR);
     if (this.gameBoard.isNotOver()) {
-      for (Alien alien : this.gameBoard.getAliens()) {
-        if (alien.getStatus()) drawBall(alien.getX(), alien.getY(), Constants.ALIEN_SIZE, g);
-      }
-
-      drawPlayer(
-          gameBoard.getPlayerX(),
-          gameBoard.getPlayerY(),
-          Constants.PLAYER_LENGTH,
-          Constants.PLAYER_HEIGHT,
-          g);
-      drawScore(gameBoard.getScore(), Constants.GAME_BOARD_WIDTH - 40, g);
-      g.setColor(Constants.BULLET_COLOR);
-      for (Bullet bullet : this.gameBoard.getBullets()) {
-        if (bullet.getStatus()) drawBall(bullet.getX(), bullet.getY(), Constants.BULLET_SIZE, g);
-      }
+      this.drawPlayer(gameBoard.getPlayerX(), gameBoard.getPlayerY(), g);
+      this.drawScore(gameBoard.getScore(), Constants.GAME_BOARD_WIDTH - 40, g);
+      this.drawAliens(g);
+      this.drawBullets(g);
     } else {
-      String message = "Game Over!    Final Score: " + gameBoard.getScore();
-      g.drawChars(
-          message.toCharArray(),
-          0,
-          message.length(),
-          Constants.GAME_BOARD_WIDTH / 2 - 80,
-          Constants.GAME_BOARD_HEIGHT / 2 - 20);
+      this.drawGameOverMessage(g);
     }
   }
 
-  private void drawPlayer(int x, int y, int width, int height, Graphics g) {
-    int centerY = y - height / 2; // (height / 2);
-    int centerX = x - (width / 2);
-    g.fillRect(centerX, centerY, width, height);
+  private void drawBullets(Graphics g) {
+    g.setColor(Constants.BULLET_COLOR);
+    for (Bullet bullet : this.gameBoard.getBullets()) {
+      if (bullet.getStatus()) drawBall(bullet.getX(), bullet.getY(), Constants.BULLET_SIZE, g);
+    }
+  }
+
+  private void drawAliens(Graphics g) {
+    g.setColor(Constants.FOREGROUND_COLOR);
+    for (Alien alien : this.gameBoard.getAliens()) {
+      if (alien.getStatus()) drawBall(alien.getX(), alien.getY(), Constants.ALIEN_SIZE, g);
+    }
+  }
+
+  private void drawGameOverMessage(Graphics g) {
+    String message = "Game Over!    Final Score: " + gameBoard.getScore();
+    g.drawChars(
+        message.toCharArray(),
+        0,
+        message.length(),
+        Constants.GAME_BOARD_WIDTH / 2 - 80,
+        Constants.GAME_BOARD_HEIGHT / 2 - 20);
+  }
+
+  private void drawPlayer(int x, int y, Graphics g) {
+    int centerY = y - Constants.PLAYER_HEIGHT / 2;
+    int centerX = x - Constants.PLAYER_LENGTH / 2;
+    g.fillRect(centerX, centerY, Constants.PLAYER_LENGTH, Constants.PLAYER_HEIGHT);
   }
 
   private void drawBall(int x, int y, int diameter, Graphics g) {
